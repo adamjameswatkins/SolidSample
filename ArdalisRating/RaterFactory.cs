19c5afab008413f7316222d5659756d7b0196d79
Adam Watkins <adamjwatkins@gmail.com>
2020-10-11 17:37:21 +0100

    Use reflection to create the Rater Types

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -1,21 +1,21 @@
+using System;
+
 namespace ArdalisRating
 {
     public class RaterFactory
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            try
             {
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
-                case PolicyType.Flood:
-                    return new FloodPolicyRater(engine, engine.Logger);
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-                default:
-                return new UnknownPolicyRater(engine, engine.Logger);
+                return (Rater)Activator.CreateInstance(
+                    Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"),
+                    new object[] { engine, engine.Logger }
+                );
+            }
+            catch (System.Exception)
+            {
+                return null;
             }
         }
     }

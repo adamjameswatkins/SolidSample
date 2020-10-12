e4c6e9e80e2032b76d19d27af5de86eaeba6ef24
Adam Watkins <adamjwatkins@gmail.com>
2020-10-12 13:33:17 +0100

    Tidy project structure Created folders and moved files Deleted unused files

--- ArdalisRating.Tests/AutoPolicyRaterRate.cs
+++ ArdalisRating.Tests/AutoPolicyRaterRate.cs
@@ -1,17 +1,8 @@
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
 namespace ArdalisRating.Tests
 {
-    public class FakeLogger : ILogger
-    {
-        public List<string> LoggedMessages { get; } = new List<string>();
-        public void Log(string message)
-        {
-            LoggedMessages.Add(message);
-        }
-    }
 
     public class AutoPolicyRaterRate
     {
